2341b26ed7c86defa44ff7a04f99a37cce04a5aa
Matt Lucas <anonuser42@hotmail.com>
2024-12-02 03:02:14 +0000

    Overcomplicated day 1

--- Day01.cs
+++ Day01.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace AdventCode2024
+﻿namespace AdventCode2024
 {
     [TestClass]
     public class Day01
@@ -16,28 +11,19 @@ namespace AdventCode2024
         {
             var (left, right, _) = values;
 
-            var result = Sort(left)
-                .Zip(Sort(right))
-                .Sum(p => Math.Abs(p.First - p.Second));
+            var result = left.Order()
+                             .Zip(right.Order())
+                             .Sum(p => Math.Abs(p.First - p.Second));
 
             Assert.AreEqual(result, 1223326);
-            return;
-
-            int[] Sort(int[] input) => input
-                .Select((v, i) => (value: v, index: i))
-                .OrderBy(p => p.value)
-                .ThenBy(p => p.index).Select(p => p.value).ToArray();
         }
         
         [TestMethod]
         public void Problem2()
         {
             var (left, right, _) = values;
-            var leftGroup =left.GroupBy(x => x);
-            var rightGroup = right.GroupBy(x => x);
+            long result = left.Sum(l => l * right.Count(r => r == l));
             
-            long result = leftGroup.Sum(g => g.Key * g.Count() * rightGroup.FirstOrDefault(g2 => g2.Key == g.Key)?.Count() ?? 0);
-
             Assert.AreEqual(result, 21070419);
         }
     }
