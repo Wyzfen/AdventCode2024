06ded7b27908b9bb77ad191be59be608cd63e3c4
Matt Lucas <anonuser42@hotmail.com>
2024-12-03 09:10:59 +0000

    Day 03

---
+++ Day03.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventCode2024
+{
+
+    [TestClass]
+    public class Day03
+    {
+        private static readonly string day = System.Reflection.MethodBase.GetCurrentMethod()!.DeclaringType!.Name.ToLower();
+        private readonly string values = String.Join("", Utils.FromFile<string>($"{day}.txt").ToArray());
+
+        private string test = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+            
+        private static Regex regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+        [TestMethod]
+        public void Problem1()
+        {
+            Regex regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+            var x = regex.Match(test);
+            int result = regex.Matches(values)
+                .Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value));
+
+            Assert.AreEqual(result, 167090022);
+        }
+
+        [TestMethod]
+        public void Problem2()
+        {
+            Regex regex = new Regex(@"(?<op>mul)\((\d{1,3}),(\d{1,3})\)|(?<op>do)\(\)|(?<op>don\'t)\(\)");
+
+            bool active = true;
+            int result = regex.Matches(values)
+                .Sum(m =>
+                {
+                    switch (m.Groups["op"].Value)
+                    {
+                        case "mul":
+                            return active ?  int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value) : 0;
+                        case "do": 
+                            active = true;
+                            break;  
+                        case "don't":
+                            active = false; 
+                            break;
+                    }
+
+                    return 0;
+                });
+
+            Assert.AreEqual(result, 89823704);
+        }
+    }
+}
