8206d75cebc7644c164e86aff73e80c51e0370c1
Matt Lucas <anonuser42@hotmail.com>
2024-12-09 21:47:19 +0000

    Day09 Part 2

--- Day09.cs
+++ Day09.cs
@@ -43,56 +43,79 @@ namespace AdventCode2024
             
             return (files, empty);
         }
-        
+
         [TestMethod]
         public void Problem1()
         {
-            checked
+            long result = 0;
+
+            var (files, free) = GetFiles(values);
+
+            IEnumerator<FileIndex> freeSpace = free.GetEnumerator();
+            freeSpace.MoveNext();
+            var (index, count) = freeSpace.Current;
+
+            for (int fileId = files.Count - 1; fileId >= 0; fileId--)
             {
-                long result = 0;
+                int fileLength = files[fileId].Length;
 
-                var (files, free) = GetFiles(values);
-
-                IEnumerator<FileIndex> freeSpace = free.GetEnumerator();
-                freeSpace.MoveNext();
-                var (index, count) = freeSpace.Current;
-                
-                for (int fileId = files.Count - 1; fileId >= 0; fileId--)
+                for (; fileLength > 0; fileLength--)
                 {
-                    int fileLength = files[fileId].Length;
-                    
-                    for(; fileLength > 0; fileLength--)
-                    {
-                        if (index >= files[fileId].Index) break;
-                        
-                        result += index++ * fileId;
+                    if (index >= files[fileId].Index) break;
 
-                        if (--count == 0 && freeSpace.MoveNext())
-                        {
-                            (index, count) = freeSpace.Current;
-                        }
-                    }
-                    
-                    if (fileLength > 0) // Ran out of space to the left - checksum in place
+                    result += index++ * fileId;
+
+                    if (--count == 0 && freeSpace.MoveNext())
                     {
-                        index = files[fileId].Index;
-                        for (; fileLength > 0; fileLength--, index++)
-                        {
-                            result += index * fileId;
-                        }
+                        (index, count) = freeSpace.Current;
                     }
                 }
 
-                Assert.AreEqual(result, 6341711060162);
+                if (fileLength > 0) // Ran out of space to the left - checksum in place
+                {
+                    index = files[fileId].Index;
+                    for (; fileLength > 0; fileLength--, index++)
+                    {
+                        result += index * fileId;
+                    }
+                }
             }
+
+            Assert.AreEqual(result, 6341711060162);
         }
 
         [TestMethod]
         public void Problem2()
         {
-            int result = 0;
+            long result = 0;
 
-            Assert.AreEqual(result, 4267809);
+            var (files, free) = GetFiles(values);
+
+            for (int fileId = files.Count - 1; fileId >= 0; fileId--)
+            {
+                var (index, fileLength) = files[fileId];
+                int freeIndex = free.FirstIndex(f => f.Length >= fileLength && f.Index < index);
+                if (freeIndex >= 0) 
+                {
+                    index = free[freeIndex].Index;
+                    int newLength = free[freeIndex].Length - fileLength;
+                    if (newLength > 0)
+                    {
+                        free[freeIndex] = new FileIndex(index + fileLength, newLength);
+                    }
+                    else
+                    {
+                        free.RemoveAt(freeIndex);    
+                    }
+                }
+
+                for (; fileLength > 0; fileLength--, index++)
+                {
+                    result += index * fileId;
+                }
+            }
+
+            Assert.AreEqual(result, 6377400869326);
         }
     }
 }
