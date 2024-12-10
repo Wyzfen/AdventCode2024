405f3b7b845b9de32046b9f28715559c288ac96c
Matt Lucas <anonuser42@hotmail.com>
2024-12-10 15:50:53 +0000

    Day10

---
+++ Day10.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventCode2024
+{
+    [TestClass]
+    public class Day10
+    {
+        private static readonly string day = System.Reflection.MethodBase.GetCurrentMethod()!.DeclaringType!.Name.ToLower();
+        readonly string [] values = File.ReadAllLines($"{day}.txt");
+
+        private readonly string[] test = new[]
+        {
+            "89010123",
+            "78121874",
+            "87430965",
+            "96549874",
+            "45678903",
+            "32019012",
+            "01329801",
+            "10456732",
+        };
+        
+        private static Vector2 [] Recurse(string [] values, int x, int y, int target)
+        {
+            if (x < 0 || x >= values[0].Length || y < 0 || y >= values.Length) return [];
+                
+            int value = values[y][x] - '0';
+                
+            if (value != target) return [];
+            if (target == 9) return [new Vector2(x, y)];
+                
+            target++;
+
+            return new []{
+                    Recurse(values, x, y + 1, target),
+                    Recurse(values, x, y - 1, target), 
+                    Recurse(values, x + 1, y, target),
+                    Recurse(values, x - 1, y, target)
+            }.SelectMany(v => v).ToArray();
+        }
+        
+        [TestMethod]
+        public void Problem1()
+        {           
+            int result = 0;
+            for (int y = 0; y < values.Length; y++)
+            {
+                for (int x = 0; x < values[0].Length; x++)
+                {
+                    if (values[y][x] == '0')
+                    {
+                        result += Recurse(values, x, y, 0).Distinct().Count();
+                    }
+                }
+            }
+
+            Assert.AreEqual(result, 644);            
+        }
+
+        [TestMethod]
+        public void Problem2()
+        {           
+            int result = 0;
+            for (int y = 0; y < values.Length; y++)
+            {
+                for (int x = 0; x < values[0].Length; x++)
+                {
+                    if (values[y][x] == '0')
+                    {
+                        result += Recurse(values, x, y, 0).Count();
+                    }
+                }
+            }
+
+            Assert.AreEqual(result, 1366);            
+        }
+    }
+}
