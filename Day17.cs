1cceb4201b15f11a959be8d5b7db591c53c70534
Matt Lucas <anonuser42@hotmail.com>
2024-12-17 22:17:40 +0000

    Day17 - work on problem 2

--- Day17.cs
+++ Day17.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventCode2024
@@ -12,9 +13,9 @@ namespace AdventCode2024
         private readonly Program test = new(729, 0, 0, [0, 1, 5, 4, 3, 0]);
         private readonly Program test2 = new(2024, 0, 0, [0, 3, 5, 4, 3, 0]);
 
-        private record struct Program(int A, int B, int C, int[] instructions)
+        private record struct Program(ulong A, ulong B, ulong C, int[] instructions)
         {
-            private enum OpCodes : int
+            private enum OpCodes
             {
                 adv,
                 bxl,
@@ -28,16 +29,15 @@ namespace AdventCode2024
 
             public IEnumerable<int> Execute() 
             {
-                int a = A, b = B, c = C;
+                ulong a = A, b = B, c = C;
 
                 int Combo(int operand) =>
                     operand switch
                     {
                         <4 => operand,
-                        4 => a,
-                        5 => b,
-                        6 => c,
-                        _ => -1
+                        4 => (int)a,
+                        5 => (int)b,
+                        6 => (int)c
                     };
                 
                 for(var sp = 0; sp < instructions.Length; sp += 2)
@@ -49,10 +49,10 @@ namespace AdventCode2024
                             a >>= Combo(operand);
                             break;
                         case OpCodes.bxl:
-                            b ^= operand;
+                            b ^= (ulong)operand;
                             break;
                         case OpCodes.bst:
-                            b = Combo(operand) % 8;
+                            b = (ulong) Combo(operand) % 8;
                             break;
                         case OpCodes.jnz:
                             if (a != 0) sp = operand - 2;
@@ -83,48 +83,51 @@ namespace AdventCode2024
             Assert.AreEqual(result, "2,1,0,4,6,2,4,2,0" );
         }
 
+        //             ulong a = Convert.ToInt64("3774103210000000", 8);
+
         [TestMethod]
         public void Problem2b()
         {
             var program = values;
-            int a = Convert.ToInt32("377410300", 8);
-            var result = string.Join(',', (program with {A = a}).Execute());
-            Assert.AreEqual(result, string.Join(',', program.instructions));
+            
+            void Recurse(int x, int[] input)
+            {
+                if (x == input.Length)
+                {
+                    Console.WriteLine(string.Join(",", input));
+                    Assert.Fail();
+                }
+                
+                var programInstructions = program.instructions[^(x+1)..];
+                
+                for (var v = 0; v < 8; v++)
+                {
+                    input[x] = v;
+                    
+                    var str = string.Join("", input);
+                    var a = Convert.ToUInt64(str, 8);
+                    
+                    var result = (program with {A = a}).Execute().ToArray();
+                    if (result.Length <= x) continue;
+                    var items = result[^(x + 1)..];
+                    
+                    if (items.SequenceEqual(programInstructions))
+                    {
+                        Console.WriteLine($"Digit {x}, using {v} works, {str} -> {string.Join("", items)}");
+                        Recurse(x + 1, input);
+                    }
+                }
+            }
+
+            Recurse(0, new int[program.instructions.Length]);
         }
 
         [TestMethod]
         public void Problem2()
         {
             var program = test2;
-            int length = program.instructions.Length;
-            int a = 0;
-            
-            while(a < int.MaxValue)
-            {
-                var inter = (program with {A = a}).Execute().GetEnumerator();
-                using var inter1 = inter as IDisposable;
-
-                int index = 0;
-                
-                while (inter.MoveNext())
-                {
-                    var output = inter.Current;
-                    
-                    // if (index == 0) Console.Write($"{a} -> {output}");
-                    // else Console.Write($", {output}");
-                    if (index >= length || output != program.instructions[index])
-                    {
-                        index = -1;
-                        break;
-                    }
-                    index++;
-                }
-                
-                //Console.WriteLine();
-
-                if (index == length) break;
-                a++;
-            }
+            long length = program.instructions.Length;
+            long a = 0;
 
             Assert.AreEqual(a, 117440); //  14892282347106 too low,
                                         // 119138258776848 too high
